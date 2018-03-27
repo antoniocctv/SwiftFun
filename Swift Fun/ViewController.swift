371795fcc9b898ce0ab0adc853a2cae88d0e3a4d
Mac <mac@Macs-iMac.local>
2018-03-26 19:01:46 -0700

    Our First Commit at Gitters

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+ 
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+        view.backgroundColor = UIColor.red
+        
+        myLabel.text = "Tony is cool Hit it more then 10 times"
+        }
+        
+        if buttonCount >= 12 {
+            view.backgroundColor = UIColor.green
+            
+            myLabel.text = "Tony is cool Hit it more then 12 times"
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
